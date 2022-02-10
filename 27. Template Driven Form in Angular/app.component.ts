861e995bc7cf4e01e1eefa71179905954cb54319
subrata <subratabca1997@gmail.com>
2022-02-09 21:25:56 -0800

    initial

---
+++ 27. Template Driven Form in Angular/app.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+
+/*
+                            Template Driven Form in Angular
+
+
+When we talking about template driven form, name attribute must be need and ngModel is used bind the data.
+
+
+==> app.module.ts
+
+import { FormsModule } from '@angular/forms';
+
+imports: [
+    FormsModule
+  ],
+
+==> app.component.html
+
+<form #simpleForm="ngForm" (ngSubmit)="getUserValue(simpleForm.value)">
+    <input type="text" name="userName" ngModel placeholder="Enter Name..."> <br>
+    <input type="text" name="age" ngModel placeholder="Enter Age..."> <br>
+    <input type="text" name="mobile" ngModel placeholder="Enter Mobile..."> <br>
+    <input type="submit">
+</form>
+
+==> app.component.ts
+
+getUserValue(val: any){
+    console.warn(val);
+  }
+
+==> index.html
+
+<body>
+  <app-root></app-root>
+</body>
+
+
+
+*/
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularProject';
+
+  ngOnInit(): void {
+  }
+
+  getUserValue(val: any){
+    console.warn(val);
+  }
+
+}
+
+
